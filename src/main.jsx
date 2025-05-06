7fcc44fee205c802476970af95d1b588e37a8c36
anatech25 <aoumenour@gmail.com>
2025-05-06 06:49:49 -0400

    added third comment

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import './index.css';
 // This is important!
 
 //This is to check the pull request
-
+//This is the third change
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
