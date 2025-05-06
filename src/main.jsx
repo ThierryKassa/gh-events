a4e7ec2e73792f7b4ef44dde96b8fd4ae48cbbea
Anani <bigkatiess2@gmail.com>
2025-05-06 07:35:21 -0400

    added comments [skip ci]

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-//Tests
-
+//Tests!
+//Add yet another test
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
